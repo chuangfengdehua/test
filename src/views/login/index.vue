9b7cc540394e9bc7c12516c0de681a1b4f431259
andy <17315599591@163.com>
2020-04-09 00:06:55 +0800

    修改了home文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,10 @@
 <!--
  * @Author: nichao
  * @Date: 2020-04-04 13:27:42
- * @LastEditTime: 2020-04-04 13:27:42
+ * @LastEditTime: 2020-04-09 00:02:16
  -->
+<template>
+  <div>
+    我是小明修改了文件
+  </div>
+</template>
