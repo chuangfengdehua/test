fde75c487e209c1c4370335cdab72a6295241d0c
andy <17315599591@163.com>
2020-04-09 23:29:18 +0800

    修改文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,11 @@
 <!--
  * @Author: nichao
  * @Date: 2020-04-04 13:27:42
- * @LastEditTime: 2020-04-09 00:34:33
+ * @LastEditTime: 2020-04-09 23:28:09
  -->
 <template>
   <div>
     我是小红修改了文件
-    我是小明修改了文件反反复复烦烦烦
+<p>lala</p>
   </div>
 </template>
