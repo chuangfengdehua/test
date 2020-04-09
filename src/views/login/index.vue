8fa433f021f6c0d89e80c019cefd007dbb6cab10
andy <17315599591@163.com>
2020-04-09 23:39:34 +0800

    修改了login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,21 @@
 <!--
  * @Author: nichao
  * @Date: 2020-04-04 13:27:42
- * @LastEditTime: 2020-04-09 23:28:09
+ * @LastEditTime: 2020-04-09 23:37:34
  -->
 <template>
   <div>
-    我是小红修改了文件
-<p>lala</p>
+    <div>woyaozengjiawenjian</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
